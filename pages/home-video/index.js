056b0ba6227a8446327572f02cf69a8a83171d2e
lijie <lijiecn@vip.qq.com>
2022-03-06 15:41:29 +0800

    async 请求

--- pages/home-video/index.js
+++ pages/home-video/index.js
@@ -1,69 +1,20 @@
 // pages/home-video/index.js
 import {getTopMV} from '../../service/api_videos.js';
 Page({
-
-    /**
-     * 页面的初始数据
-     */
     data: {
         topMVs: []
     },
 
     /**
      * 生命周期函数--监听页面加载
+     * async await 同步编写异步代码，但是可能会导致页面卡顿
      */
-    onLoad: function (options) {
-        getTopMV(0).then(res => {
-            this.setData({ topMVs: res.data });
-        });
+    onLoad: async function (options) {
+        const res = await getTopMV(0);
+        this.setData({topMVs: res.data});
+        // getTopMV(0).then(res => {
+        //     this.setData({ topMVs: res.data });
+        // });
     },
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
-    }
 })
