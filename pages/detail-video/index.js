4a4a8029008882a36bfc4da55afbeb407024568e
lijie <lijiecn@vip.qq.com>
2022-03-07 19:40:15 +0800

    get mv pag data

--- pages/detail-video/index.js
+++ pages/detail-video/index.js
@@ -1,71 +1,45 @@
 // pages/detail-video/index.js
+import { getMvURL, getMvDetail, getMvRelatedVideo } from '../../service/api_videos';
 Page({
 
     /**
      * 页面的初始数据
      */
     data: {
-
+        mvURL: {},
+        mvDetail: {},
+        relatedVideo: []
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-        console.log('options by navigate', options);
+        const id = options.id;
+        this.getPageData(id);
+    },
+
+    getPageData: function (id) {
         // 1. 请求视频地址
-
+        getMvURL(id).then(res => {
+            console.log('getMvURL', res);
+            this.setData({
+                mvURL: res.data.url
+            });
+        })
         // 2. 请求视频信息
-
+        getMvDetail(id).then(res => {
+            console.log('getMvDetail', res);
+            this.setData({
+                mvDetail: res.data
+            });
+        })
         // 3. 请求相关视频
-    },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
+        getMvRelatedVideo(id).then(res => {
+            console.log('getMvRelatedVideo', res);
+            this.setData({
+                relatedVideo: res.data
+            });
+        })
     }
 })
--- service/api_videos.js
+++ service/api_videos.js
@@ -3,3 +3,31 @@ import NETWORK from "./index";
 export function getTopMV(offset) {
     return NETWORK.get('/top/mv', { offset, limit: 10 });
 }
+
+
+/**
+ * 根据 歌曲id 获取 MV 视频播放地址
+ * @param {number} id 歌曲 id
+ * @returns 
+ */
+export function getMvURL(id) {
+    return NETWORK.get('/mv/url', { id: id });
+}
+
+/**
+ * 根据 视频 id 获取视频详情
+ * @param {number} mvid 
+ * @returns 
+ */
+export function getMvDetail(mvid) {
+    return NETWORK.get('/mv/detail', { mvid });
+}
+
+/**
+ * 根据 歌曲 id 获取相关的 MV 推荐
+ * @param {number} id 歌曲 id
+ * @returns 
+ */
+export function getMvRelatedVideo(id) {
+    return NETWORK.get('/related/allvideo', { id });
+}
