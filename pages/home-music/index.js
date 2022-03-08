ee56e46f1a4cf9b4042ad7cae2f1f5c74d04f255
lijie <lijiecn@vip.qq.com>
2022-03-08 18:50:54 +0800

    throttle version of query image height

--- pages/home-music/index.js
+++ pages/home-music/index.js
@@ -1,6 +1,10 @@
 // pages/home-music/index.js
 import { getBanners } from '../../service/api_music'
 import queryElement from '../../utils/query_element'
+import throttle from '../../utils/throttle'
+
+const throttleQuery = throttle(queryElement)
+
 Page({
     data: {
         banners: [],
@@ -23,11 +27,16 @@ Page({
     imgLoaded: function () {
         console.log('img loaded');
         // 获取元素的高度
-        queryElement(".swiper-item-image").then(res => {
+        throttleQuery(".swiper-item-image").then(res => {
             this.setData({ swiperHeight: res[0].height });
-        })
-
-
+            // 节流新版本
+            console.log('throttle version');
+        });
+        // queryElement(".swiper-item-image").then(res => {
+        //     this.setData({ swiperHeight: res[0].height });
+        //     // 原始版本
+        //     console.log('orignal version');
+        // })
     },
     handleSearchClick: function () {
         console.log('handleSearchClick');
--- utils/query_element.js
+++ utils/query_element.js
@@ -2,7 +2,7 @@ export default function (selector) {
     return new Promise((resolve, reject) => {
         const query = wx.createSelectorQuery();
         query.select(selector).boundingClientRect();
-        query(exec(resolve, reject));
+        query.exec(resolve, reject);
     });
 
 }
