66c0134372dd735595e20fa6b47974c29895a508
lijie <lijiecn@vip.qq.com>
2022-03-09 19:06:11 +0800

    hot playlist scroll view

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 // app.js
 // 逻辑处理
-App({})
+App({
+    onLaunch: function () {
+        const sys = wx.getSystemInfoSync();
+        console.log('SYSTEM INFO',sys);
+        this.globalData.screenWidth = sys.screenWidth;
+        this.globalData.screenHeight = sys.screenHeight;
+    },
+    globalData: {
+        screenWidth: 0,
+        screenHeight: 0,
+    }
+})
