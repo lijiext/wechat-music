c161dd7d8a5da1011c6e9abc2fc358fed6690e83
lijie <lijiecn@vip.qq.com>
2022-03-25 23:03:10 +0800

    music player init

--- components/song-item-v1/index.js
+++ components/song-item-v1/index.js
@@ -6,5 +6,13 @@ Component({
         }
     },
     data: {},
-    methods: {}
+    methods: {
+        handleSongItemClick: function (e) {
+            console.log('handleSongItemClick from V1, song id:', e.currentTarget.dataset.id);
+            const id = e.currentTarget.dataset.id;
+            wx.navigateTo({
+                url: '/pages/music-player/index?id=' + id
+            });
+        }
+    }
 });
--- components/song-item-v2/index.js
+++ components/song-item-v2/index.js
@@ -10,5 +10,13 @@ Component({
         }
     },
     data: {},
-    methods: {}
+    methods: {
+        handleSongItemClick: function (e) {
+            console.log('handleSongItemClick from V2, song id:', e.currentTarget.dataset.id);
+            const id = e.currentTarget.dataset.id;
+            wx.navigateTo({
+                url: '/pages/music-player/index?id=' + id
+            });
+        }
+    }
 });
