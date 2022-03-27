80c6322025af7873c34c48e3d99da1c98f1339ad
lijie <lijiecn@vip.qq.com>
2022-03-27 20:33:25 +0800

    player swiper change

--- pages/music-player/index.js
+++ pages/music-player/index.js
@@ -3,13 +3,22 @@ import {getSongById} from "../../service/api_player";
 Page({
     data: {
         id: 0,
-        currentSong: {}
+        currentSong: {},
+        currentPage: 0,
+        swiperHeight: 0
     },
     onLoad: function (options) {
+        // 设置 swiper 高度
+        const screenHeight = getApp().globalData.screenHeight;
+        const statusBarHeight = getApp().globalData.statusBarHeight;
+        // 本处使用了常量写死，不建议这么做
+        const swiperHeight = screenHeight - statusBarHeight - 44;
+
         //    1. 获取传入的音乐 id
         const id = options.id;
         this.setData({
-            id
+            id,
+            swiperHeight
         });
         //    2. 获取音乐详情
         this.getPageData(id);
@@ -24,5 +33,12 @@ Page({
                 currentSong: res.songs[0]
             });
         })
+    },
+    // 监听页面切换
+    swiperChange: function (e) {
+        const currentPage = e.detail.current;
+        this.setData({
+            currentPage
+        });
     }
 });
